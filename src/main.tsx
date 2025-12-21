5a1853130e892ea3739698de360d493adaff59f5
munakata255 <ba151031@icloud.com>
2025-12-21 19:25:47 +0900

    Fix: Minor main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,12 @@
-import { StrictMode } from 'react'
+// import { StrictMode } from 'react'
 import { AuthProvider } from './contexts/AuthContext.tsx'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  
     <AuthProvider>
       <App />
     </AuthProvider>
-  </StrictMode>
   )
